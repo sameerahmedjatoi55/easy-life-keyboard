ad72ac686c2f4d325dcfc7b217ba5df33163f062
Easy Life Build <build@easylife.keyboard>
2026-03-30 16:26:18 -0400

    Switch to Kotlin DSL for Gradle - fixes compatibility issues

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file using Kotlin DSL
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
